bdfd1d20da6e1c01c82ab76307e049b74f83a8e2
cuongpv01 <cuongpv01@UNI008-PC>
2024-08-06 17:17:24 +0700

    test

--- Fontend/fontend-wed/src/pages/Register.js
+++ Fontend/fontend-wed/src/pages/Register.js
@@ -47,7 +47,7 @@ const Register = () => {
           <label>Email:</label>
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit">Registers</button>
       </form>
       {error && <p>{error}</p>}
     </div>
